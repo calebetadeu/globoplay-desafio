ce7e28f91283938251cc0945984b4b7a3f73b753
calebetadeu@gmail.com <calebetadeu@gmail.com>
2025-04-12 17:30:29 -0300

    removendo imports desnecessário

--- app/src/main/java/com/calebetadeu/globoplaychallenge/MainActivity.kt
+++ app/src/main/java/com/calebetadeu/globoplaychallenge/MainActivity.kt
@@ -3,19 +3,10 @@ package com.calebetadeu.globoplaychallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.navArgs
-import com.calebetadeu.globoplaychallenge.core.presentation.components.BottomBarGloboPlay
-import com.calebetadeu.globoplaychallenge.core.presentation.components.GloboPlayTopBar
-import com.calebetadeu.globoplaychallenge.home.presentation.HomeRoot
 import com.calebetadeu.globoplaychallenge.navigation.NavigationApp
 import com.calebetadeu.globoplaychallenge.ui.theme.GloboPlayChallengeTheme
 
